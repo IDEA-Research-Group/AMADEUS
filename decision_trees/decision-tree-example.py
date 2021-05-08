f46f17deb90b84b1412331aad9bd7fc97bd1b99f
Nicolás de Ory <deorynicolas@gmail.com>
2021-05-08 13:20:21 +0200

    Implemented AHP weighing

--- decision_trees/decision-tree-example.py
+++ decision_trees/decision-tree-example.py
@@ -1,4 +1,6 @@
 import ahpy
+import jsonpickle
+from ahp_structure import AhpNode, AhpTree
 
 '''
 Ejemplo. Tenemos los siguientes criterios y subcriterios
@@ -27,40 +29,37 @@ debian
     - debian-debian_linux-version-8_0
 '''
 
-# Criterios raiz. Como de importante es A respecto de B. 1 = misma importancia
-criteria_comparisons = {
-    ('apache','openssl'): 1/2,
-    ('apache','canonical'): 2,
-    ('apache','debian'): 2,
-    ('openssl','canonical'): 2,
-    ('openssl','debian'): 2,
-    ('canonical','debian'): 2,
-}
+def process_node(comparisons, node, parentId = None):
+    if isinstance(node, AhpTree):
+        nodeId = 'root'
+        for child in reversed(node.get_children()):
+            process_node(comparisons, child, 'root')
+            return
+    else:
+        nodeId = node.id
 
-apache_version_comparison = {
-    ('apache-http_server-version-2_0_0', 'apache-http_server-version-2_1_0'): 1/2,
-    ('apache-http_server-version-2_0_0', 'apache-http_server-version-2_2_0'): 1/4,
-    ('apache-http_server-version-2_1_0', 'apache-http_server-version-2_2_0'): 1/2
-}
+    comparisons[nodeId] = {}
+    comparisons[nodeId]['parent'] = parentId;
+    comparisons[nodeId]['comparisons'] = {}
+    comparisons[nodeId]['children'] = []
 
-openssl_version_comparison = {
-    ('openssl-openssl-version_0_9_4', 'openssl-openssl-version_0_9_7b'): 1/2,
-    ('openssl-openssl-version_0_9_4', 'openssl-openssl-version_0_9_8'): 1/4,
-    ('openssl-openssl-version_0_9_7b', 'openssl-openssl-version_0_9_8'): 1/2
-}
+    for comparedId, value in node.comparisons.items():
+        comparisons[nodeId]['comparisons'][(nodeId, comparedId)] = value
 
-ubuntu_version_comparison = {
-    ('canonical-ubuntu_linux-version-10_04', 'canonical-ubuntu_linux-version-12_04'): 1/2,
-    ('canonical-ubuntu_linux-version-10_04', 'canonical-ubuntu_linux-version-13_04'): 1/4,
-    ('canonical-ubuntu_linux-version-12_04', 'canonical-ubuntu_linux-version-13_04'): 1/2
-}
+    for child in reversed(node.get_children()):
+        comparisons[nodeId]['children'].append(child.id)
+        process_node(comparisons, child, nodeId)
 
-debian_version_comparison = {
-    ('debian-debian_linux-version-4_0', 'debian-debian_linux-version-6_0'): 1/2,
-    ('debian-debian_linux-version-4_0', 'debian-debian_linux-version-8_0'): 1/4,
-    ('debian-debian_linux-version-6_0', 'debian-debian_linux-version-8_0'): 1/2
-}
+with open('./decision_trees/result-afm-ahp.json') as file:
+    s = file.read()
+    ahpTree = jsonpickle.decode(s)
 
+comparisons = {}
+
+process_node(comparisons, ahpTree)
+print(jsonpickle.encode(comparisons))
+
+'''
 root = ahpy.Compare('Criteria',criteria_comparisons, precision=4, random_index='saaty')
 apache_version = ahpy.Compare('apache-version', apache_version_comparison, precision=4, random_index='saaty')
 openssl_version = ahpy.Compare('openssl-version', openssl_version_comparison, precision=4, random_index='saaty')
@@ -78,16 +77,43 @@ debian.add_children([debian_version])
 
 print("ROOT")
 print(root.target_weights)
+'''
 
-# Una vez tenemos los target weights, definimos las alternativas
-# y las evaluamos para determinar la más apropiada
+finalComparisons = {}
+for key, item in comparisons.items():
+    if item['parent'] == 'root':
+        continue
+    if not item['parent'] in finalComparisons:
+        finalComparisons[item['parent']] = {(key,key):1}
+    
+    if any(item['comparisons']) and any(x for x in finalComparisons[item['parent']].keys() if x[0] == x[1]):
+        finalComparisons[item['parent']] = {}
 
-alt1 = ['openssl-openssl-version_0_9_8','apache-http_server-version-2_0_0','debian-debian_linux-version-6_0']
-alt2 = ['openssl-openssl-version_0_9_8','apache-http_server-version-2_0_0','canonical-ubuntu_linux-version-13_04']
-alt3 = ['openssl-openssl-version_0_9_4','apache-http_server-version-2_1_0','canonical-ubuntu_linux-version-13_04']
+    for compKey, compVal in item['comparisons'].items():
+        finalComparisons[item['parent']][compKey] = compVal
 
-for i, alt in enumerate([alt1,alt2,alt3]):
-    tot = sum(1*root.target_weights[attr] for attr in alt)
-    print("Alternative {} = {}".format(i+1, tot))
+# Right now multiple children in root aren't handled
+rootObject = list(comparisons.items())[0]
+rootKey = rootObject[0]
 
-# Los resultados indican que la alternativa 2 es la mas deseable.
+root = ahpy.Compare(rootKey, finalComparisons[rootKey], precision=4, random_index='saaty')
+finalAhpCompareObjects = {rootKey: {'compareObj': root, 'children': []}}
+
+for key, item in comparisons.items():
+    parent = item['parent']
+    if parent == 'root':
+        continue
+    
+    if key in finalComparisons:
+        compareObject = ahpy.Compare(key, finalComparisons[key], precision=4, random_index='saaty')
+
+        finalAhpCompareObjects[key] = {'compareObj': compareObject, 'children': []}
+        finalAhpCompareObjects[parent]['children'].append(compareObject)
+
+for key, item in finalAhpCompareObjects.items():
+    if len(item['children']) > 0:
+        item['compareObj'].add_children(item['children'])
+
+
+print("ROOT")
+print(root.target_weights)
