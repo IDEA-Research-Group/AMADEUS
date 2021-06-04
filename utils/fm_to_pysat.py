e17b21a1c9d5d1eceaf5cbfc06fe6e3d95292ec6
GermanOctako <germanoctako@gmail.com>
2021-06-04 19:37:54 +0200

    CNF transform finished

--- utils/fm_to_pysat.py
+++ utils/fm_to_pysat.py
@@ -129,193 +129,95 @@ class FmToPysat(ModelToModel):
                     result.append(cnf_left)
                 for cnf_rigth in cnfs_rigth:
                     result.append(cnf_rigth)
-            elif actual_op == 'or':
-                aux = []
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
                 for result1 in cnfs_left:
                     for result2 in cnfs_rigth:
-                        aux.append(self.flatten([result1,result2]))
-                result.extend(aux)
-            elif actual_op in ('requires','implies'):
-                for obj in cnfs_rigth:
-                    if isinstance(obj, list):
-                        cnf = [-cnfs_left]
-                        cnf.extend(obj)
-                        result.append(cnf)
-                    elif isinstance(obj, int):
-                        cnf = [-cnfs_left]
-                        cnf.append(obj)
-                        result.append(cnf)
+                        result.append(self.flatten([result1,result2]))
         elif isinstance(cnfs_left, int) and isinstance(cnfs_rigth, list):
             if actual_op == 'and':
                 result.append([cnfs_left])
                 result.extend(self.add_and_cnfs(cnfs_rigth))
-            elif actual_op == 'or':
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
                 for cnf_rigth in cnfs_rigth:
                     cnf = self.flatten([cnfs_left, cnf_rigth])
                     result.append(cnf)
-            elif actual_op in ('requires','implies'):
-                for obj in cnfs_rigth:
-                    if isinstance(obj, list):
-                        cnf = [cnfs_left]
-                        cnf.extend(obj)
-                        result.append(cnf)
-                    elif isinstance(obj, int):
-                        cnf = [cnfs_left]
-                        cnf.append(obj)
-                        result.append(cnf)
-            elif actual_op == 'excludes':
-                for obj in cnfs_rigth:
-                    if isinstance(obj, list):
-                        cnf = [cnfs_left]
-                        cnf.extend(obj)
-                        result.append(cnf)
-                    elif isinstance(obj, int):
-                        cnf = []
-                        cnf.append(obj)
-                        cnf.append(cnfs_left)
-                        result.append(cnf)
         elif isinstance(cnfs_left, list) and isinstance(cnfs_rigth, int):
             if actual_op == 'and':
                 result.append(cnfs_rigth)
                 result.extend(self.add_and_cnfs(cnfs_left))
-            elif actual_op == 'or':
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
                 for cnf_left in cnfs_left:
                     cnf = self.flatten([cnf_left, cnfs_rigth])
                     result.append(cnf)
-            elif actual_op in ('requires','implies'):
-                for obj in cnfs_left:
-                    if isinstance(obj, list):
-                        cnf = [x for x in obj]
-                        cnf.append(cnfs_rigth)
-                        result.append(cnf)
-                    elif isinstance(obj, int):
-                        cnf = []
-                        cnf.append(obj)
-                        cnf.append(cnfs_rigth)
-                        result.append(cnf)
-            elif actual_op == 'excludes':
-                for obj in cnfs_left:
-                    if isinstance(obj, list):
-                        cnf = [x for x in obj]
-                        cnf.append(cnfs_rigth)
-                        result.append(cnf)
-                    elif isinstance(obj, int):
-                        cnf = []
-                        cnf.append(obj)
-                        cnf.append(cnfs_rigth)
-                        result.append(cnf)
         else: #left int and right int
             if actual_op == 'and':
                 result.append([cnfs_left])
                 result.append([cnfs_rigth])
-            elif actual_op == 'or':
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
                 result = [[cnfs_left,cnfs_rigth]]
-            elif actual_op == 'excludes':
-                result = [[cnfs_left,cnfs_rigth]]
-            elif actual_op in ('requires','implies'):
-                result = [[cnfs_left,cnfs_rigth]]
-
         return result
 
-    def negative_ast_iterator(self, ctc, node):
-        result = []
-        print(node.get_name())
-        name = node.get_name().replace('(','').replace(')','')
-        childs = ctc.ast.get_childs(node)
-        if name == 'and':
-            cnfs_left = self.negative_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.negative_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, 'or')
-        elif name == 'or':
-            cnfs_left = self.negative_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.negative_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, 'and')
-        elif name in ('requires', 'implies'):
-            cnfs_left = self.positive_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.negative_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, name)
-        elif name == 'excludes':
-            cnfs_left = self.positive_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.positive_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, name)
-        elif name == 'not':
-            var = self.destination_model.variables.get(
-                ctc.ast.get_childs(node)[0].get_name()
-            )
-            if var:
-                result = var
-            else:
-                cnfs = self.positive_ast_iterator(ctc, childs[0])
-                for cnf in cnfs:
-                    result.append(cnf)
-        else:
-            var = self.destination_model.variables.get(node.get_name())
-            result = - var
-        print(result)
-        return result
-
-    def positive_ast_iterator(self, ctc, node):
-        result = []
-        print(node.get_name())
-        name = node.get_name().replace('(','').replace(')','')
-        childs = ctc.ast.get_childs(node)
-        if name == 'and':
-            cnfs_left = self.positive_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.positive_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, name)
-        elif name == 'or':
-            cnfs_left = self.positive_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.positive_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, name)
-        elif name in ('requires', 'implies'):
-            cnfs_left = self.negative_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.positive_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, name)
-        elif name == 'excludes':
-            cnfs_left = self.negative_ast_iterator(ctc, childs[0])
-            cnfs_rigth = self.negative_ast_iterator(ctc, childs[1])
-            result = self.combinator(cnfs_left, cnfs_rigth, name)
-        elif name == 'not':
-            var = self.destination_model.variables.get(
-                ctc.ast.get_childs(node)[0].get_name()
-            )
-            if var:
-                result = - var
-            else:
-                cnfs = self.negative_ast_iterator(ctc, childs[0])
-                for cnf in cnfs:
-                    aux = [- x for x in cnf]
-                    result.append(aux)
-        else:
-            var = self.destination_model.variables.get(node.get_name())
-            result = var
-        print(result)
-        return result
-
-    def add_constraint(self, ctc):
+    def ast_iterator(self, ctc, node, number):
         '''
-        Hay dos iteradores debido a que las reglas de las leyes de Morgan cambian los operadores or y and
-        cuando se usar el operador not para negar una clausula. Debido a que esta negación es necesaría
-        al trasformar a CNF nacen el iterador positivo y negativo que se complementan en el flujo de 
-        creación del CNF.
+        La variable number se utiliza para seguir las leyes de morgan expuestas a continuación.
         Reglas de las leyes de Morgan:
             A <=> B      = (A => B) AND (B => A)
             A  => B      = NOT(A) OR  B
             NOT(A AND B) = NOT(A) OR  NOT(B) 
             NOT(A OR  B) = NOT(A) AND NOT(B) 
         '''
-        node = ctc.ast.get_root()
+        result = []
+        print(node.get_name())
         name = node.get_name()
-        if name == 'not':
-            cnfs = self.negative_ast_iterator(ctc, node)
-        elif name in ('and', 'or', 'requires', 'excludes', 'implies'):
-            cnfs = self.positive_ast_iterator(ctc, node)
+        childs = ctc.ast.get_childs(node)
+        if name == 'and':
+            cnfs_left = self.ast_iterator(ctc, childs[0], number)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number)
+            result = self.combinator(cnfs_left, cnfs_rigth, name if number > 0 else 'or')
+        elif name == 'or':
+            cnfs_left = self.ast_iterator(ctc, childs[0], number)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number)
+            result = self.combinator(cnfs_left, cnfs_rigth, name if number > 0 else 'and')
+        elif name in ('requires', 'implies'):
+            cnfs_left = self.ast_iterator(ctc, childs[0], number * - 1)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number)
+            result = self.combinator(cnfs_left, cnfs_rigth, name)
+        elif name == 'excludes':
+            cnfs_left = self.ast_iterator(ctc, childs[0], number * - 1)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number * - 1)
+            result = self.combinator(cnfs_left, cnfs_rigth, name)
+        elif name == 'not':
+            var = self.destination_model.variables.get(
+                ctc.ast.get_childs(node)[0].get_name()
+            )
+            if var:
+                result = - var * number
+            else:
+                cnfs = self.ast_iterator(ctc, childs[0], number * - 1)
+                result = cnfs
+        else:
+            var = self.destination_model.variables.get(node.get_name())
+            result = var * number
+        print(result)
+        return result
+
+    def add_constraint(self, ctc):
+        node = ctc.ast.get_root()
+        print(ctc.ast)
+        if node.get_name() in ('and', 'or', 'requires', 'excludes', 'implies', 'not'):
+            cnfs = self.ast_iterator(ctc, node, 1)
         else:
             print('This FM contains non supported elements', file=sys.stderr)
 
-        for cnf in cnfs:
-            print(cnf)
-            self.cnf.append(cnf)
+        if isinstance(cnfs, list):
+            for cnf in cnfs:
+                if isinstance(cnf, list):
+                    self.cnf.append(cnf)
+                else:
+                    self.cnf.append([cnf])
+        else:
+            print(cnfs)
+            self.cnf.append([cnfs])
 
     def transform(self):
         for feature in self.source_model.get_features():
